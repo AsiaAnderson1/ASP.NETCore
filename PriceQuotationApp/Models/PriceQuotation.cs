d83462978e02b46e6ad37b2d9673d9d2ccbc0496
Asia A <52732384+AsiaAnderson1@users.noreply.github.com>
2020-09-17 19:21:51 -0400

    Add files via upload

---
+++ PriceQuotationApp/Models/PriceQuotation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Tasks;
+
+namespace PriceQuotationApp.Models
+{
+    public class PriceQuotation
+    {
+        public decimal subtotal { get; set; }
+        public decimal discountPercent { get; set; }
+        public decimal discountAmount { get; set; }
+        public decimal calculateTotal()
+        {
+            decimal discountAmount = subtotal * discountPercent;
+            decimal total = subtotal - discountAmount;
+            return total; 
+        }
+        
+
+    }
+}
